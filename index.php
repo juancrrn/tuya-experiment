2a93fcfd9b4b7c83b4c097ea0dc8356a8373291e
Juan Carrión <hola@juancrrn.io>
2021-03-07 17:41:36 +0100

    Added Bootstrap dependencies

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ require_once __DIR__ . '/app/Utils.php';
 	<head>
 		<meta charset="utf-8">
 		<title>Tuya Smart WiFi meter data decoding experiment</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/style.css">
 	</head>
 	<body>
@@ -68,5 +69,7 @@ foreach ($globalStatistics->getSearchOccurencesMap() as $occurence) {
 				</ul>
 			</li>
 		</ul>
+		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
 	</body>
 </html>
