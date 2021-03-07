942e31d21234b8766cd273b672843879b27940a4
Juan Carrión <hola@juancrrn.io>
2021-03-07 21:04:25 +0100

    Added highlighting mode to coloring explanation

--- templates/aboutTheFollowingTables.php
+++ templates/aboutTheFollowingTables.php
@@ -6,10 +6,18 @@
             <span class="std eqcol">1</span> (no color): the value never changes in the column.
         </div>
         <div class="col-md">
-            <span class="std common">1</span> (gray): the value changes some time in the column.
+            <span class="std common">1</span> (green):
+            <ul>
+                <li><code>TE_HIGHLIGHT_MODE = true</code>: the value changes some time in the column.</li>
+                <li><code>TE_HIGHLIGHT_MODE = false</code>: 0-valued bits.</li>
+            </ul>
         </div>
         <div class="col-md">
-            <span class="std diff">1</span> (purple): the value is different to the one in the previous row.
+            <span class="std diff">1</span> (purple):
+            <ul>
+                <li><code>TE_HIGHLIGHT_MODE = true</code>: the value is different to the one in the previous row.</li>
+                <li><code>TE_HIGHLIGHT_MODE = false</code>: 1-valued bits.</li>
+            </ul>
         </div>
     </div>
 
