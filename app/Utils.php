accbe76f437c8a517bdde3b650d4bcbce8cccd8e
Juan Carrión <hola@juancrrn.io>
2021-03-12 16:01:04 +0100

    Added a third option of string-to-binary decoder

--- app/Utils.php
+++ app/Utils.php
@@ -3,10 +3,8 @@
 class Utils
 {
     
-    public static function stringToBinaryA7($string): string
+    public static function stringToBinaryA7(string $string): string
     {
-        $string = base64_encode($string);
-
         $l = strlen($string);
         $result = '';
 
@@ -17,7 +15,7 @@ class Utils
         return $result;
     }
 
-    public static function stringToBinaryB7($string): string
+    public static function stringToBinaryB7(string $string): string
     {
         $characters = str_split($string);
         $binary = [];
@@ -28,6 +26,50 @@ class Utils
         }
     
         return implode('', $binary);    
+    }
+
+    public static function stringToBinaryC(string $string): string
+    {
+        /**
+         * 1. Ensure that the string is UTF-8-encoded.
+         * 
+         * 2. Convert each character of the string to a number between 0 and
+         * 255 (format code 'C' with repeater argument '*'). This is an
+         * interpretation of the character as an unsigned integer in that
+         * range. Other formats are available at the PHP Manual:
+         *
+         * https://www.php.net/manual/en/function.pack.php
+         * 
+         * The result of unpack() is an array containing the representations
+         * of all the characters. E. g., for 'COEAA8MAAMo=':
+         * 
+         * [67, 79, 69, 65, 65, 56, 77, 65, 65, 77, 111, 61]
+         */
+        $byteArray = unpack('C*', utf8_encode($string));
+            var_dump($byteArray);
+
+        /**
+         * 3. Prepare a variable to store the concatenated string.
+         */
+        $result = '';
+
+        /**
+         * 4. Loop through each representation.
+         */
+        foreach ($byteArray as $byte) {
+            /**
+             * 5. Convert the representation from 0 to 255, to a pure binary
+             * representation.
+             * 
+             * 6. As 8 bits are needed to represent the range 0 to 255, pad
+             * the binary representation with leading 0s.
+             * 
+             * 7. Add the representation to the result string.
+             */
+            $result .= str_pad(decbin($byte), 8, '0', STR_PAD_LEFT);
+        }
+
+        return $result;   
     }
 }
 
